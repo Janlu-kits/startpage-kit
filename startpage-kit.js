23a39396c1f92f920ccf3bbd3667084b5989aeb7
JanluOfficial <95766563+JanluOfficial@users.noreply.github.com>
2024-07-02 12:18:32 +0200

    Move code

--- startpage-kit.js
+++ startpage-kit.js
@@ -77,14 +77,3 @@ class ShortcutContainer {
         return;
     }
 }
-
-function classic(pos) {
-    var shortcuts = new ShortcutContainer()
-    shortcuts.shortcuts = ["www.youtube.com", ["Maps", "https://maps.google.com/"], "www.you215tube.com", "w125ww.youtube.com"];
-    shortcuts.id = "containerXD"
-    shortcuts.direction = "h";
-    shortcuts.styleType = "button";
-    shortcuts.wrapperVisibility = true;
-    shortcuts.position = pos;//"center";
-    shortcuts.build()
-}
---
+++ startpage.js
@@ -0,0 +1,8 @@
+var shortcuts = new ShortcutContainer()
+    shortcuts.shortcuts = ["www.youtube.com", ["Maps", "https://maps.google.com/"], "www.you215tube.com", "w125ww.youtube.com"];
+    shortcuts.id = "containerXD"
+    shortcuts.direction = "h";
+    shortcuts.styleType = "button";
+    shortcuts.wrapperVisibility = true;
+    shortcuts.position = "center";
+    shortcuts.build()
